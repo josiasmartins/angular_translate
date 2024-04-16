252f1ac62ddff893a70be32ef32815cf43a889d2
Caitano <josias.m.caitano@accenture.com>
2024-04-15 21:21:31 -0300

    feature: create page not_found and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // { path: "", component:  }
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { 
+        path: "home", 
+        loadComponent: () => import("./pages/home/home.component").then(c => c.HomeComponent) 
+    },
+    {
+        path: "**",
+        loadComponent: () => import("./pages/not_found/not_found.component").then(c => c.NotFoundComponent)
+    }
 ];
---
+++ src/app/pages/not_found/not_found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './not_found.component.html',
+  styleUrl: './not_found.component.css'
+})
+export class NotFoundComponent {
+
+}
